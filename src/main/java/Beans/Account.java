ccb49c0c15aabecffeade144ed37a04aff08f3f9
amayer99 <alex.mayer@cgi.com>
2022-07-28 14:10:53 -0400

    bean-boilerplate: --add account, customer, transaction --update properties

---
+++ src/main/java/Beans/Account.java
@@ -0,0 +1,48 @@
+package Beans;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountId;
+
+    @Column(nullable = false, length=10)
+    private String accountType;
+
+    @Column(nullable = false)
+    private Long currentBalance;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "customerId", nullable = false) 
+    private Customer customer;
+
+    @OneToMany(mappedBy = "sendCustomer", fetch = FetchType.EAGER,
+    cascade = CascadeType.ALL)
+    private Set<Transaction> sentTransactions;
+
+    @OneToMany(mappedBy = "recieveCustomer", fetch = FetchType.EAGER,
+    cascade = CascadeType.ALL)
+    private Set<Transaction> recievedTransactions;
+}
---
+++ src/main/java/Beans/Transaction.java
@@ -0,0 +1,36 @@
+package Beans;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "transactions")
+
+public class Transaction{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "sendCustomerId", nullable = false)
+    private Customer sendCustomer;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "recieveCustomerId", nullable = false)
+    private Customer recieveCustomer;
+}
